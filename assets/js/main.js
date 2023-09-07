c7b568304526aab647384c4dff35e0977ab55834
Bete.mrt <bete.mrt@gmail.com>
2023-09-07 14:29:54 -0300

    :lipstick: feat: cards size

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 // function createTypesLi(types) {
 //   return types.map((object) => `<li class="type">${object.type.name}</li>`);
 // }
+let limit = 1281;
+let offset = 0;
 
 function createPokemon(pokemon) {
   return `
@@ -25,4 +27,22 @@ pokeApi.getPokemonList().then((pokemonList = []) => {
   // pokemonOl.innerHTML += createPokemon(pokemon); 
   const pokemonOl = document.querySelector('.pokemons');
   pokemonOl.innerHTML = pokemonList.map(createPokemon).join('');
+});
+
+const btnNext = document.querySelector('#btnNext');
+btnNext.addEventListener('click', () => {
+  offset += 10;
+  pokeApi.getPokemonList(offset, 10).then((pokemonList = []) => {
+    const pokemonOl = document.querySelector('.pokemons');
+    pokemonOl.innerHTML = pokemonList.map(createPokemon).join('');
+  });
+});
+
+const btnPrev = document.querySelector('#btnPrev');
+btnPrev.addEventListener('click', () => {
+  offset -= 10;
+  pokeApi.getPokemonList(offset, 10).then((pokemonList = []) => {
+    const pokemonOl = document.querySelector('.pokemons');
+    pokemonOl.innerHTML = pokemonList.map(createPokemon).join('');
+  });
 });
